80ead0a3337643b7729f7ed8442842d5e3ee7c04
Kameswara Gandru <kameswararao313209@gmail.com>
2023-01-29 11:53:01 +0530

    Initial Commit

---
+++ src/test/java/basic/FirstTest.java
@@ -0,0 +1,11 @@
+package basic;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void firstMethod(){
+        System.out.println("This is my first test");
+    }
+}
