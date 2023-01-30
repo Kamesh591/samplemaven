f45bd85961b466c33d230c124524a0e98ec2cf80
Kameswara Gandru <kameswararao313209@gmail.com>
2023-01-30 23:14:48 +0530

    Updated AssetTrue statement on Test

--- src/test/java/basic/FirstTest.java
+++ src/test/java/basic/FirstTest.java
@@ -1,11 +1,14 @@
 package basic;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirstTest {
 
     @Test
     public void firstMethod(){
+
         System.out.println("This is my first test");
+        Assert.assertTrue(false);
     }
 }
