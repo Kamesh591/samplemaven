5607eb1e25681a80f3c0b02f86140527f487cc27
Kameswara Gandru <kameswararao313209@gmail.com>
2023-02-02 06:44:04 +0530

    Additional test cased added

--- src/test/java/basic/FirstTest.java
+++ src/test/java/basic/FirstTest.java
@@ -1,14 +1,28 @@
 package basic;
 
+import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirstTest {
 
-    @Test
+    @Test(description = "First Test case for Allure Implementation")
+    @Description("This is an allure report test")
     public void firstMethod(){
 
         System.out.println("This is my first test");
         Assert.assertTrue(false);
+    }
+
+@Test(description = "This is 2nd test case")
+@Description("2nd Test Allure Report")
+    public void secondMethod(){
+        System.out.println("Second Test");
+    }
+
+    @Test(description = "This is 3rd test case")
+    @Description("3rd Test Allure Report")
+    public void thirdMethod(){
+        System.out.println("Second Test");
     }
 }
